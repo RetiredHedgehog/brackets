4979ad740fa4bf2255580e7e084751c18da39236
Nikita Lukashov <56773398+RetiredHedgehog@users.noreply.github.com>
2021-02-15 01:04:22 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str, bracketsConfig);
+  
+  if ( str.length % 2 !== 0 ) return false;
+
+  bracketsConfig = bracketsConfig.map(elem => (elem + '').replace(/,/g, ''));
+  
+  let oldLength;
+  let newLength;
+
+  do {
+    oldLength = str.length;
+    
+    bracketsConfig.forEach(elem => {
+      if ( str.indexOf(elem) > -1 ) {
+        str = str.slice(0, str.indexOf(elem)) + str.slice(str.indexOf(elem) + 2);
+      }
+    });
+    
+    newLength = str.length;
+  } while(newLength !== oldLength);
+
+  if ( str.length === 0 ) return true;
+  return false;
 }
